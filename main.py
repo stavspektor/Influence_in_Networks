53ba78b3c976ced8f8c650c495eb4cd76cc2d9b0
stavbhm <stav.barhaim@gmail.com>
2022-05-01 13:49:49 +0300

    J,

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ instaglam0_df = pd.read_csv('instaglam0.csv')
 instaglam_1_df = pd.read_csv('instaglam_1.csv')
 artist_df = pd.read_csv('spotifly.csv')
 
+
 # Build the graphs
 G0 = nx.from_pandas_edgelist(instaglam0_df, 'userID', 'friendID')
 G_1 = nx.from_pandas_edgelist(instaglam_1_df, 'userID', 'friendID')
@@ -15,33 +16,54 @@ G_1 = nx.from_pandas_edgelist(instaglam_1_df, 'userID', 'friendID')
 Diff = nx.difference(G0, G_1)
 """
 
-# set node attributes: 'purchase'=True/False
-nx.set_node_attributes(G0, False, name="purchase")
+# Set node attributes: 'purchase'=True/False
+nx.set_node_attributes(G0, False, name="purchase_70")
+nx.set_node_attributes(G0, False, name="purchase_150")
+nx.set_node_attributes(G0, False, name="purchase_989")
+nx.set_node_attributes(G0, False, name="purchase_16326")
 #G0.nodes[145]['purchase'] = True
 #print(G0.nodes[31383]['purchase'])
 
 
-# setting node attributes: 'listened'=True/False
-our_artists = (artist_df[' artistID'] == 70) | (artist_df[' artistID'] == 150) | (artist_df[' artistID'] == 989) | (
-            artist_df[' artistID'] == 16326)
-our_artists_df = artist_df[our_artists]
-nx.set_node_attributes(G0, False, name="listened")
-for node in G0.node():
-    if node in our_artists_df['userID']:
-        G0.nodes[node]['listened'] = True
-########## maybe 4 attributes of listen one for every artist
-########## and 4 filters
+# Setting node attributes number of plays for every artist: 'listened_ID'
+artist_70 = artist_df[' artistID'] == 70
+artist_150 = artist_df[' artistID'] == 150
+artist_989 = artist_df[' artistID'] == 989
+artist_16326 = artist_df[' artistID'] == 16326
+
+artist_70_df = artist_df[artist_70]
+artist_150_df = artist_df[artist_150]
+artist_989_df = artist_df[artist_989]
+artist_16326_df = artist_df[artist_16326]
+
+nx.set_node_attributes(G0, 0, name="listened_70")
+nx.set_node_attributes(G0, 0, name="listened_150")
+nx.set_node_attributes(G0, 0, name="listened_989")
+nx.set_node_attributes(G0, 0, name="listened_16326")
+
+for node in G0.nodes():
+    if node in list(artist_70_df.loc[:, 'userID']):
+        G0.nodes[node]['listened_70'] = artist_70_df.loc[int((artist_70_df['userID'] == node).index.tolist()[0]), '#plays']
+
+    if node in list(artist_150_df.loc[:, 'userID']):
+        G0.nodes[node]['listened_150'] = artist_150_df.loc[int((artist_150_df['userID'] == node).index.tolist()[0]), '#plays']
+
+    if node in list(artist_989_df.loc[:, 'userID']):
+        G0.nodes[node]['listened_989'] = artist_989_df.loc[int((artist_989_df['userID'] == node).index.tolist()[0]), '#plays']
+
+    if node in list(artist_16326_df.loc[:, 'userID']):
+        G0.nodes[node]['listened_16326'] = artist_16326_df.loc[int((artist_16326_df['userID'] == node).index.tolist()[0]), '#plays']
 
 
-# analyze the edge creation probability
+# Analyze the edge creation probability
 sum_common_hist = [0] * (G_1.number_of_nodes() - 2)
 sum_real_hist = [0] * (G_1.number_of_nodes() - 2)
 for user1 in nx.nodes(G_1):
     for user2 in nx.nodes(G_1):
-        if user1 != user2 and G_1.has_edge(user1,user2) is False:
+        if user1 != user2 and G_1.has_edge(user1, user2) is False:
             sum_common = len(list(nx.common_neighbors(G_1, user1, user2)))
             sum_common_hist[sum_common] += 1
-            if G0.has_edge(user1,user2) is True:
+            if G0.has_edge(user1, user2) is True:
                 sum_real_hist[sum_common] += 1
 
 probability_list = [0] * (G_1.number_of_nodes() - 2)
@@ -51,12 +73,14 @@ for i in range(len(sum_common_hist)):
     else:
         probability_list[i] = 0
 
+print(probability_list)
 
 # Simulation until t=6
-Graph = G0
-Graph_prev = G0
-for i in range(6):
-    Graph_prev = Graph
+Graph = G0.copy()
+for i in range(1, 7):
+    print('i =', i)
+    Graph_prev = Graph.copy()
+    # Building the new Graph
     for user1 in nx.nodes(Graph):
         for user2 in nx.nodes(Graph):
             if user1 != user2 and Graph.has_edge(user1, user2) is False:
@@ -64,18 +88,60 @@ for i in range(6):
                 if random.random() < probability_list[sum_common]:
                     Graph.add_edge(user1, user2)
 
+    # Calculating the purchase probabilities and update purchase per artist for every node
+    for user in nx.nodes(Graph):
+        Bt_70 = 0
+        Bt_150 = 0
+        Bt_989 = 0
+        Bt_16326 = 0
+        Nt = len(list(nx.neighbors(Graph, user)))
+        for neighbor in nx.neighbors(Graph, user):
+            if Graph_prev.nodes[neighbor]['purchase_70']:
+                Bt_70 += 1
+            if Graph_prev.nodes[neighbor]['purchase_150']:
+                Bt_150 += 1
+            if Graph_prev.nodes[neighbor]['purchase_989']:
+                Bt_989 += 1
+            if Graph_prev.nodes[neighbor]['purchase_16326']:
+                Bt_16326 += 1
 
+        # For artist 70
+        if Graph.nodes[user]['listened_70'] != 0:
+            purchase_prob_70 = (Graph.nodes[user]['listened_70'] * Bt_70) / (1000 * Nt)
+        else:
+            purchase_prob_70 = Bt_70 / Nt
+        if random.random() < purchase_prob_70:
+            Graph.nodes[user]['purchase_70'] = True
+
+        # For artist 150
+        if Graph.nodes[user]['listened_150'] != 0:
+            purchase_prob_150 = (Graph.nodes[user]['listened_150'] * Bt_150) / (1000 * Nt)
+        else:
+            purchase_prob_150 = Bt_150 / Nt
+        if random.random() < purchase_prob_150:
+            Graph.nodes[user]['purchase_150'] = True
+
+        # For artist 989
+        if Graph.nodes[user]['listened_989'] != 0:
+            purchase_prob_989 = (Graph.nodes[user]['listened_989'] * Bt_989) / (1000 * Nt)
+        else:
+            purchase_prob_989 = Bt_989 / Nt
+        if random.random() < purchase_prob_989:
+            Graph.nodes[user]['purchase_989'] = True
+
+        # For artist 16326
+        if Graph.nodes[user]['listened_16326'] != 0:
+            purchase_prob_16326 = (Graph.nodes[user]['listened_16326'] * Bt_16326) / (1000 * Nt)
+        else:
+            purchase_prob_16326 = Bt_16326 / Nt
+        if random.random() < purchase_prob_16326:
+            Graph.nodes[user]['purchase_16326'] = True
+
+    print('end i =', i)
 
 
 
 #filter = new.groupby('userID').size() == 2
-
-
-
-
-
-
-
 
 
 
